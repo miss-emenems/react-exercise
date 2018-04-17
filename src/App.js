3b8c8628b6679360ccdccf146e7fabf2bbd9a581
Martyna <martyna.machowiak@gmail.com>
2018-04-17 22:39:49 +0100

    Add json file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Location from './components/Location';
+import Results from './components/Results';
 
 class App extends Component {
   render() {
@@ -16,8 +17,7 @@ class App extends Component {
         </p>
         <div className="finder">
           <Location />
-
-
+          <Results />
         </div>
       </div>
     );
--- src/components/Results.js
+++ src/components/Results.js
@@ -2,15 +2,29 @@
  * Created by Trilian on 17/04/2018.
  */
 import React from "react";
+import ResultsItem from "./ResultsItem";
+import results from "../data/results.json";
 
 const Results = () => {
+	//const tempArray = [1, 2, 3, 4];
 	return (
 		<div className="results">
-			<ul className="list list--shops">
-				<li class="list__item">
-					<h2>1</h2>
-				</li>
-			</ul>
+			<div className="results__list">
+				<ul className="list list--shops">
+					{
+						results.map((result, i) => {
+							return (
+								<ResultsItem key={ i } result={ result } />
+							)
+						})
+					}
+				</ul>
+			</div>
+			<div className="results__map">
+				<div className="map">
+
+				</div>
+			</div>
 		</div>
 	)
 }
