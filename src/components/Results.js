acbdd82d46af1155a24232ffa805c46b18db94c0
Martyna <martyna.machowiak@gmail.com>
2018-04-17 20:30:54 +0100

    Add Loaction form and initial markup for results

---
+++ src/components/Results.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Trilian on 17/04/2018.
+ */
+import React from "react";
+
+const Results = () => {
+	return (
+		<div className="results">
+			<ul className="list list--shops">
+				<li class="list__item">
+					<h2>1</h2>
+				</li>
+			</ul>
+		</div>
+	)
+}
+
+export default Results;
