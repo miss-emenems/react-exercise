01cfc6dfc9061e12cfd9e9a6ce0a450d921e29fd
Martyna <martyna.machowiak@gmail.com>
2018-04-21 20:22:04 +0100

    Change search results into buttons and add styling for them on hover and focus

--- src/components/ResultsItem.js
+++ src/components/ResultsItem.js
@@ -7,17 +7,19 @@ import React from "react";
 const ResultsItem = (props) => {
 	const result = props.result;
 	return (
-		<div className="grid-child height-fr box--frame">
-			<div className="results__item item">
-				<span className="item__number">{ result.id }</span>
-				<h3 className="item__name">{ result.name }</h3>
-				<address className="item__address">
-					{ result.firstLine },<br/>
-					{ result.city },<br/>
-					{ result.postcode }
-				</address>
-				<p className="item__distance">distance goes here</p>
-			</div>
+		<div className="grid-child height-fr">
+			<button className="btn--box btn--full">
+				<div className="results__item item">
+					<span className="item__number invert">{ result.id }</span>
+					<h3 className="item__name">{ result.name }</h3>
+					<address className="item__address">
+						{ result.firstLine },<br/>
+						{ result.city },<br/>
+						{ result.postcode }
+					</address>
+					<p className="item__distance">distance goes here</p>
+				</div>
+			</button>
 		</div>
 	)
 }
