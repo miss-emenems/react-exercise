4827ee40e63b7ff3657411bbed78b21731f05bd6
Martyna <martyna.machowiak@gmail.com>
2018-04-21 20:02:35 +0100

    Refactor json file and item for results

--- src/components/ResultsItem.js
+++ src/components/ResultsItem.js
@@ -12,7 +12,9 @@ const ResultsItem = (props) => {
 				<span className="item__number">{ result.id }</span>
 				<h3 className="item__name">{ result.name }</h3>
 				<address className="item__address">
-					{ result.address },<br/>{ result.postcode }
+					{ result.firstLine },<br/>
+					{ result.city },<br/>
+					{ result.postcode }
 				</address>
 				<p className="item__distance">distance goes here</p>
 			</div>
